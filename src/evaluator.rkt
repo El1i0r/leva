1c3f47cb76ae20a1263a9c0d59b4afb695f5458a
Affan Ahmad <st_iaffan@outlook.com>
2025-10-28 15:49:25 +0300

    feat(leva): BROKEN: attempted to transfer to typed/racket.

--- src/evaluator.rkt
+++ src/evaluator.rkt
@@ -1,15 +1,45 @@
-#lang racket
+#lang typed/racket
+
+#| Essential Types: |#
+
+;; Environment
+(define-type Env (List (Pair String CLOS)))
+
+;; Expression
+(define-type Expr
+  (U
+    (Var)
+    (Abs)
+    (App)))
+
+; Struct for variable
+(struct Var 
+  ([name : String]))
+
+; Struct for lambda abstraction (function)
+(struct Abs 
+  ([param : String]
+   [body : Expr]))
+
+; Struct for function application
+(struct App 
+  ([func : Expr]
+   [arg : Expr]))
 
 #| Closures:
   A closure packages an expression that has not yet been evaluated
   with the run-time environment in which the expression was created
 |#
-(struct CLOS (env var body) #:transparent)
+(struct CLOS ([env : Env] [var : Symbol] [body : Expr]) #:transparent)
 
-(define (extend ρ x v)
+;; Extends an environment
+(define (extend [ρ : Env] [x : String] [v : CLOS])
   (cons (cons x v) ρ))
 
-(define (val ρ e)
+#| Evaluation:
+  Evaluates a expression given a environment ρ, and expression e.
+|#
+(define (evaluate ρ e)
   (match e
     [`(λ (,x) ,b)
      (CLOS ρ x b)]
@@ -19,21 +49,21 @@
            (cdr xv)
            (error 'val "Unknown variable ~a" x)))]
     [`(,rator ,rand)
-     (do-ap (val ρ rator) (val ρ rand))]))
+     (do-ap (evaluate ρ rator) (evaluate ρ rand))]))
 
 (define (do-ap clos arg)
   (match clos
     [(CLOS ρ x b)
-     (val (extend ρ x arg) b)]))
+     (evaluate (extend ρ x arg) b)]))
 
 (define (run-program ρ exprs)
   (match exprs
     ['() (void)]
     [(cons `(define ,x ,e) rest)
-     (let ([v (val ρ e)])
+     (let ([v (evaluate ρ e)])
        (run-program (extend ρ x v) rest))]
     [(cons e rest)
-     (displayln (val ρ e))
+     (displayln (evaluate ρ e))
      (run-program ρ rest)]))
 
 (define (add-* x)
